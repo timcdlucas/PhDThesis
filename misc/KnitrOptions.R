3296f1b6892794f31cd6d73c5ab885dedee23ae9
timcdlucas <timcdlucas@gmail.com>
2015-07-21 10:42:15 +0100

    Figures and ggplot theme working in knitr.

--- misc/KnitrOptions.R
+++ misc/KnitrOptions.R
@@ -1,15 +1,23 @@
 
+
+.libPaths(c('~/R/x86_64-pc-linux-gnu-library/', .libPaths()))
+
 propOfTextwidth = 0.8
 
-opts_chunk$set(echo = FALSE, 
-  fig.width = 6, 
-  fig.height = 6, 
+opts_chunk$set(
+  echo = FALSE, 
   cache = 2, 
   dev = 'cairo_pdf', 
   warning = FALSE,
-  results = FALSE,
+  results = 'hide',
   message = FALSE,
   fig.width = 6.45 * propOfTextwidth,
+  fig.height = 6.45 * propOfTextwidth * 0.8,
   fig.align = 'center',
-  out.width=paste0(propOfTextwidth,'\\textwidth'))
+  fig.pos = 't',
+  #fig.showtext = TRUE, # Embeds fonts.
+  out.width = paste0(propOfTextwidth,'\\textwidth')
+  )
+
+
 
