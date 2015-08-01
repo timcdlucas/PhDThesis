9b5fad90c3567afc439656dbde3cd6586c6b4493
Tim Lucas <timcdlucas@gmail.com>
2015-08-01 17:24:54 +0100

    Starting chapter3 document.

--- misc/KnitrOptions.R
+++ misc/KnitrOptions.R
@@ -2,26 +2,31 @@
 
 .libPaths(c('~/R/x86_64-pc-linux-gnu-library/', .libPaths()))
 
+# Default figure width
 propOfTextwidth = 0.8
 
 opts_chunk$set(
-  echo = FALSE, 
+  echo = TRUE, 
   cache = FALSE, 
   dev = 'cairo_pdf', 
   warning = FALSE,
   results = 'hide',
   message = FALSE,
   fig.width = 6.45 * propOfTextwidth,
-  fig.height = 6.45 * propOfTextwidth * 0.8,
+  fig.height = 6.45 * propOfTextwidth * 0.7,
   fig.align = 'center',
   fig.pos = 't',
   #fig.showtext = TRUE, # Embeds fonts.
-  out.width = paste0(propOfTextwidth,'\\textwidth')
+  out.width = paste0(propOfTextwidth,'\\textwidth'),
+  dev.args=list(bg="transparent"),
+  size = 'footnotesize'
   )
 options(digits=2)
 
 
 
+
+# Try and get knitr to plot numbers nicely without manually tuning each number
 
 # Write a function for pvalues
 p <- function(x, eps = 1e-3){
@@ -44,5 +49,14 @@ p <- function(x) return(x)
 
 
 options('scipen' = -1)
+
+
+# syntax highlighting.
+# This is only used for echo = TRUE which won't be in the final thesis at all.
+
+knit_theme$set("camo")
+
+
+
 
   
--- misc/theme_tcdl.R
+++ misc/theme_tcdl.R
@@ -17,11 +17,11 @@ theme_tcdl <-theme(text = element_text(family = "Lato Light", size = 12),
         #axis.ticks.y = element_blank(),
         panel.background = element_blank(),
         legend.title = element_text(size = 16, colour  =  "#8B8B8B"),
-        legend.key = element_rect(fill = "white"),
+        legend.key = element_rect(fill = "white", colour = NA),
         legend.key.size = unit(1.2, "lines"),
         legend.text = element_text(size = 15, colour  =  "#8B8B8B"),
         axis.text = element_text(color = "grey", size = 13, family  =  "Lato Black"),
-        axis.title = element_text(size = 22),
+        axis.title = element_text(size = 18),
         axis.title.y = element_text(vjust = 2.5),
         axis.title.x = element_text(vjust = -1),
         title = element_text(size = 22),
@@ -30,4 +30,36 @@ theme_tcdl <-theme(text = element_text(family = "Lato Light", size = 12),
         plot.margin  =  unit(c(0.3,0.1,1,1.3), "lines"))
 
 theme_set(theme_grey() + theme_tcdl)
+
+
+theme_pres <- theme(text = element_text(family = "Lato Light", size = 12),
+        panel.grid.major.x = element_line(colour = "#ECECEC", size = 0.3, linetype = 1),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(colour = "#ECECEC", size = 0.3, linetype = 1),
+        legend.title = element_text(size = 16, colour  =  "#8B8B8B"),
+        legend.key = element_rect(fill = "transparent", colour = NA),
+        legend.background = element_rect(fill = "transparent", colour = NA),
+        legend.key.size = unit(1.2, "lines"),
+        legend.text = element_text(size = 18, colour  =  "#8B8B8B"),
+        axis.text = element_text(color = "darkgrey", size = 15, family  =  "Lato Black"),
+        axis.title = element_text(size = 25),
+        axis.title.y = element_text(vjust = 2.5),
+        axis.title.x = element_text(vjust = -1),
+        title = element_text(size = 22),
+        panel.border  =  element_blank(), 
+        axis.line  =  element_line(colour  =  "darkgrey"),
+        panel.background = element_rect(fill = "transparent", colour = NA), 
+        plot.background = element_rect(colour = NA, fill = 'transparent'),
+        plot.margin  =  unit(c(0.3,0.1,1,1.3), "lines")
+        )
+
+
+
+
+thesis_palette <- function(type = "seq"){
+  scale_colour_brewer(palette = 'Dark2')
+}
+
+
 
